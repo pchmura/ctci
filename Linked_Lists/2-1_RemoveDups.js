132f6ad3584a16186abe561222d03fd015f6036c
pchmura <pchmura8@gmail.com>
2018-02-06 23:17:57 -0500

    saving work

--- Linked_Lists/2-1_RemoveDups.js
+++ Linked_Lists/2-1_RemoveDups.js
@@ -1,6 +1,12 @@
 //Write code to remove duplicates from an unsorted linked list
 //Follow up: How would you solve this problem if a temporary buffer is not allowed?
 
+function generateTestList(){
+
+}
 function removeDups(list){
     
 }   
+let testNode = new Node(3);
+
+console.log(testNode);
--- Linked_Lists/LinkedList.js
+++ Linked_Lists/LinkedList.js
@@ -1,4 +1,49 @@
-function Node(data){
+function Node(data) {
     this.data = data;
     this.next = null;
 }
+
+Node.prototype.appendToTail = function (data) {
+    const newNode = new Node(data);
+    let currentNode = this;
+    while (currentNode.next) {
+        currentNode = currentNode.next;
+    }
+    currentNode.next = newNode;
+};
+
+Node.prototype.lastNode = function () {
+    let currentNode = this;
+    while (currentNode.next) {
+        currentNode = currentNode.next;
+    }
+    return currentNode;
+};
+
+Node.prototype.deleteNextNode = function () {
+    this.next = this.next.next;
+};
+
+Node.prototype.printList = function () {
+    let currentNode = this;
+    while (currentNode.next) {
+        console.log(currentNode.data);
+        currentNode = currentNode.next;
+    }
+    console.log(currentNode.data);
+};
+
+function generateTestList(length) {
+    let headOfList = new Node(0);
+    for (let i = 1; i < length; i++) {
+        headOfList.appendToTail(i);
+    }
+    return headOfList;
+}
+
+// let testList = new Node(0); testList.appendToTail(1);
+// testList.appendToTail(2); console.log(testList.lastNode())
+// testList.printList();
+
+let genList = generateTestList(10);
+genList.printList();
